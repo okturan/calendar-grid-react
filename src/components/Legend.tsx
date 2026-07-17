29e906a7744a3a5801d716146e871c2ff5b3aedb
okturan <okanert@gmail.com>
2026-07-17 14:19:42 +0200

    docs: add verified product proof and accessible filters

--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -10,15 +10,18 @@ const Legend = ({ shares, selectedShares, onToggleShare }: LegendProps) => {
   return (
     <div className="legend">
       {Object.keys(shares).map((shareId) => (
-        <div
+        <button
+          type="button"
           key={shareId}
           className={`legend-item ${selectedShares.has(shareId) ? "selected" : ""}`}
+          aria-pressed={selectedShares.has(shareId)}
+          aria-label={`Highlight ${shareId} periods`}
           onClick={() => onToggleShare(shareId)}>
           <div className={`legend-color ${shareId}`}></div>
           <span>{shareId}</span>
-        </div>
+        </button>
       ))}
-      <div className="legend-item">
+      <div className="legend-item legend-maintenance">
         <div className="legend-color maintenance"></div>
         <span>Maintenance</span>
       </div>
